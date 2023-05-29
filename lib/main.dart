38f5d5b27bf9740a72d26a960c79695aae7066aa
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2023-05-29 20:18:52 +0000

    - assets/main
    - initial setup to start the app.
    - code to show onboarding screen on app open once.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:myqr/onboarding/oboarding_screen.dart';
+// ignore: depend_on_referenced_packages
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MaterialApp(
+
+late int initScreen;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = (prefs.getInt("initScreen"))!;
+  await prefs.setInt("initScreen", 1);
+  if (kDebugMode) {
+    print('initScreen $initScreen');
+  }
+  runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: const Color.fromRGBO(216, 191, 216, 1),
+      focusColor: const Color.fromRGBO(216, 191, 216, 1),
+      buttonTheme: const ButtonThemeData(
+        buttonColor: Color.fromRGBO(216, 191, 216, 1),
+      ),iconTheme: const IconThemeData(
+        color: Color.fromRGBO(216, 191, 216, 1),
+      ),
+    ),
+    home: initScreen == 0 ? const Onboarding() : const Onboarding(),
   ));
 }
 
